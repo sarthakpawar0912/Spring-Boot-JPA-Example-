8aa66d12564f9dc99aed1dbf0842444f3ba8d9b2
Sarthak Pawar <pawarsr06@gmail.com>
2025-01-20 10:56:11 +0530

    Updated features and fixed issues

--- src/main/java/com/jpa/test/entity/User.java
+++ src/main/java/com/jpa/test/entity/User.java
@@ -18,11 +18,9 @@ public class User {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getCity() {
         return city;
     }
